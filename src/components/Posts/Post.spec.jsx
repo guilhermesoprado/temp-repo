581ac0e7df5a775d25190ea848e32fda0f30662c
Guilherme Prado <guilhermesoprado@gmail.com>
2022-12-14 12:38:44 +0000

    Configuração do eslint, prettier, editorconfig e prop-types

---
+++ src/components/Posts/Post.spec.jsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import { Post } from '.';
+
+
+describe('<Post />', () =>{
+
+    it('should render Post Card correctly', () => {
+
+
+    });
+
+});
